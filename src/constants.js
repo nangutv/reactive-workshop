fd133e042e53ede6c86826e9db641b4f324258c4
Jakub Rimal <jakub.rimal@nangu.tv>
2019-10-31 17:08:55 +0100

    bigger movies for mobile

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
-export const COVER_HEIGHT = 157;
-export const COVER_WIDTH = 105;
+import isMobile from './lib/isMobile';
+
+let COVER_HEIGHT = 157;
+let COVER_WIDTH = 105;
+
+if (isMobile()) {
+    COVER_HEIGHT = 300;
+    COVER_WIDTH = 200;
+}
+
+export { COVER_HEIGHT };
+export { COVER_WIDTH };
